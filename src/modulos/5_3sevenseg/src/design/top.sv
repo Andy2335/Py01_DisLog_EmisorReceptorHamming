0005586ee8003ed913dfe8cca2ded147269e6627
Andy2335 <andres.obregon23@gmail.com>
2026-03-29 18:33:33 -0600

    PROGRA top Seven Segment Display

--- src/modulos/5_3sevenseg/src/design/top.sv
+++ src/modulos/5_3sevenseg/src/design/top.sv
@@ -1,50 +1,67 @@
+// MODULO 5.2 - Seven Segment Display
+// Autor: Andres Obregon Lopez
+// Circuito combinacional para el display de 7 segmentos en representacion hexadecimal (0-F)
+
 module top (
-    input logic [3:0] sw,           // A (MSB), B,C,D.. Del 0 al 3 hay 4 bits
-    output logic [6:0] segments    
+    input logic [3:0] sw,           // Bits de entrada: AIN ,BIN ,CIN ,DIN (MSB a LSB)
+    output logic [6:0] segments     // Segments: SA, SB, SC, SD, SE, SF, SG (LSB a MSB)
 );
 
-    /* Esta seccion de codigo no representa nada funcional, solo se asigna un nombre a 
-    cada bit para escribir las ecuaciones boooleanas con mayor facilidad.
-    Como renombrar un nodo.     */
-assign A = sw[3];
-assign B = sw[2];
-assign C = sw[1];
-assign D = sw[0];
-    //
+/*Asignacion de los bits de entrada */
+assign AIN = sw[3];
+assign BIN = sw[2];
+assign CIN = sw[1];
+assign DIN = sw[0];
 
-    /*Esta seccion de codigo presenta la logica booleana necesaria para las salidas del sistema, 
-    las cuales se deben conectar a un respectivo segmento del display.*/
+/*Asignacion de los bits de salida */
+assign SA = segments[0];
+assign SB = segments[1];
+assign SC = segments[2];
+assign SD = segments[3];
+assign SE = segments[4];
+assign SF = segments[5];
+assign SG = segments[6];
 
-assign segments[0] = (A) |               // segmento A
-                    (B & ~C) |
-                    (~B & C) |
-                    (C & ~D);  
+/*Asignacion de operadores logicos reutilizados - NOT*/
+assign AIN_N = ~AIN;
+assign BIN_N = ~BIN;
+assign CIN_N = ~CIN;
+assign DIN_N = ~DIN;
 
-assign segments[1] = (A & ~B & ~C) |     // segmento B
-                    (~A & B & ~C) |
-                    (~A & B & ~D) |
-                    (~A & ~C & ~D); 
+/*Expresiones logicas para cada segmento*/
 
-assign segments[2] = (~A & C & ~D) |     // segmento C
-                    (~B & ~C & ~D);        
+//Segmento A: SA = Din'(Ain+Bin')+Ain'(Cin+BinDin)+BinCin+AinBin'Cin'
+assign SA = (DIN_N) & (AIN | BIN_N) | 
+            (AIN_N) & (CIN | (BIN & DIN)) | 
+            (BIN & CIN) | 
+            (AIN & BIN_N & CIN_N);  
 
-assign segments[3] = (~A & ~B & C) |     // segmento D
-                    (~A & C & ~D) |
-                    (~B & ~C & ~D) |
-                    (~A & B & ~C & D);
+//Segmento B: SB = Bin'(Ain' + Din') + Ain'(Cin ⊙ Din) + Ain Din Cin'
+assign SB = (BIN_N) & (AIN_N | DIN_N) | 
+            (AIN_N) & (~(CIN ^ DIN)) | 
+            (AIN & DIN & CIN_N);
 
-assign segments[4] = (~A & B) |          // segmento F
-                    (~A & D) |
-                    (~B & ~C);
+//Segmento C: SC = AinBin' + Ain'(Bin + Din + Cin') + DinCin'
+assign SC = (AIN & BIN_N) | 
+            (AIN_N) & (BIN | DIN | CIN_N) | 
+            (DIN & CIN_N);   
 
-assign segments[5] = (~A & ~B) |         // segmento E
-                    (~B & ~C) |
-                    (~A & C & D) |
-                    (~A & ~C & ~D); 
+//Segmento D: SD = Din'(Ain'Bin' + BinCin + AinCin') + Din(BinCin' + Bin'Cin)
+assign SD = (DIN_N & (AIN_N & BIN_N | BIN & CIN | AIN & CIN_N)) | 
+            (DIN & (BIN & CIN_N | BIN_N & CIN));
 
-assign segments[6] = (~A & C) |         // segmento G
-                    (~A & B & D) |
-                    (A & ~B & ~C) |
-                    (~A & ~B & ~D);                            
-    
+//Segmento E: SE = Din'(Bin'+Cin) + Ain(Cin + Bin)
+assign SE = (DIN_N & (BIN_N | CIN)) | 
+            (AIN & (CIN | BIN));
+
+//Segmento F: SF = Ain(Cin + Bin') + Din'(Bin + Cin') + Ain'BinCin'
+assign SF = (AIN & (CIN | BIN_N)) | 
+            (DIN_N & (BIN | CIN_N)) | 
+            (AIN_N & BIN & CIN_N);
+
+//Segmento G: SG = Ain(Din + Bin') + Cin(Bin' + Din') + Ain'BinCin'
+assign SG = (AIN & (DIN | BIN_N)) | 
+            (CIN & (BIN_N | DIN_N)) | 
+            (AIN_N & BIN & CIN_N);
+
 endmodule
