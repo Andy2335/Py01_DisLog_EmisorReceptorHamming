1814e4a3559cf9c1b3785542874a4222869c271f
Andy2335 <andres.obregon23@gmail.com>
2026-03-31 11:23:21 -0600

    Correccion Constraints

--- src/modulos/5_3sevenseg/src/sim/top_tb.sv
+++ src/modulos/5_3sevenseg/src/sim/top_tb.sv
@@ -36,12 +36,7 @@ module top_tb;
             4'd7: expected = 7'b1110000;
             4'd8: expected = 7'b1111111;
             4'd9: expected = 7'b1110011;
-            4'd10: expected = 7'b1110111; // A
-            4'd11: expected = 7'b0011111; // B
-            4'd12: expected = 7'b1001110; // C
-            4'd13: expected = 7'b0111101; // D
-            4'd14: expected = 7'b1001111; // E
-            4'd15: expected = 7'b1000111; // F
+
 
             default: expected = 7'b0000000;
         endcase
